fa4d194bb86df58ab84b362db8d0b4dc6f7edef9
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-05-17 19:53:31 -0300

    Separate content into components

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,24 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 por{' '}
+      <a
+        className="underline hover:text-gray-100"
+        href="https://github.com/joaoliveirapb"
+        target="_blank"
+        rel="noreferrer"
+      >
+        João Oliveira
+      </a>{' '}
+      no NLW da{' '}
+      <a
+        className="underline hover:text-gray-100"
+        href="https://www.rocketseat.com.br/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
