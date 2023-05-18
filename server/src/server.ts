98c8b74938223ff4244ed0cf939eb3c3b404457c
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-05-18 19:52:10 -0300

    authenticated with Github

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,5 +20,8 @@ app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
-  .listen({ port: 3333 })
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
   .then(() => console.log('🚀 HTTP server running on http://localhost:3333'))
